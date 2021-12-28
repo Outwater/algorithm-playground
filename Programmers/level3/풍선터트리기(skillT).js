66a4f99d3c1ea15a747e970d9358ffa32b180de6
Outwater <kocon135@gmail.com>
2021-12-28 12:07:51 +0900

    solve 풍선터트리기 효율성개선

--- Programmers/level3/풍선터트리기(skillT).js
+++ Programmers/level3/풍선터트리기(skillT).js
@@ -1,37 +1,45 @@
+//* 난이도 및 풀이 시간
+// 실제 난이도: lv3 체감 난이도: lv3
+//* 문제이해
+//
+//* 아이디어
+//  a만큼의 배열을 새로 만든다.
+// 해당 idx를 기준으로 왼쪽의 가장 작은 값을 모아두는 minL과 ... minR 만들기
+//* 풀이방법(순서도, 절차적프로그래밍)
+// 1. idx기준으로, 왼쪽,오른쪽으로 가장 작은 값들을 저장하는 leftMin, rightMin 배열만들기
+// 2. a를 순회하며, a[i] 가 leftMin[i], rightMin[i] 보다 하나라도 작은 값이 존재하면 cnt++
+// 3. cnt 리턴
+//* 시간복잡도
+// O(N)
+
+//* 복습필요여부
+// No
+
 function solution(a) {
-  // 번호가 더 작은 풍선을 터트리는 것은 최대 1번
-  // 그 이후로는 큰 풍선만 터트린다.
-  // 어떤풍선 최후가능 / 불가능
-  // 최후가 가능한 풍선의 개수를 return
-
-  /* point: 큰게 남기 힘들다
-   자신 기준 왼쪽 오른쪽으로 가장 작은 값을 가져왔을 때, 한쪽이라도, 자신 보다 작은 값이 없다면(혹은 비었다면) 가능
-  */
-
-  //! O(N^2)으로 효율성 실패! -> O(N)으로 줄여야함
+  // a를 순회하며, minL과 minR 보다 작은 값이라면 cnt++
   let cnt = 0;
+  let minL = a[0];
+  let minR = a[a.length - 1];
+  let leftMin = a.map((el) => {
+    if (el < minL) minL = el;
+    return minL;
+  });
+  let rightMin = a
+    .map((el, idx) => {
+      if (a[a.length - 1 - idx] < minR) {
+        minR = a[a.length - 1 - idx];
+      }
+      return minR;
+    })
+    .reverse();
 
   for (let i = 0; i < a.length; i++) {
-    let left = getLeft(a[i], i, a);
-    let right = getLight(a[i], i, a);
-    if (left || right) {
+    if (a[i] <= leftMin[i] || a[i] <= rightMin[i]) {
+      // console.log(leftMin[i],a[i],rightMin[i])
       cnt++;
     }
   }
 
-  function getLeft(value, idx, arr) {
-    let slice = arr.slice(0, idx);
-    if (slice.length === 0) return true;
-    let min = Math.min(...slice);
-    if (value < min) return true;
-    return false;
-  }
-  function getLight(value, idx, arr) {
-    let slice = arr.slice(idx + 1);
-    if (slice.length === 0) return true;
-    let min = Math.min(...slice);
-    if (value < min) return true;
-    return false;
-  }
   return cnt;
 }
+console.log(solution([-16, 27, 65, -2, 58, -92, -71, -68, -61, -33])); //6
