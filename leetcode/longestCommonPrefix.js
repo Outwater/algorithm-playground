505178203ac94373e5b9bb509e58f89a6d92a121
Insoo-Seo <kocon135@gmail.com>
2022-02-06 23:43:50 +0900

    resolve Leetcode longestCommonPrefix

--- leetcode/longestCommonPrefix.js
+++ leetcode/longestCommonPrefix.js
@@ -1,17 +1,20 @@
 //* 난이도 및 풀이 시간
-// start:
-// end:
-// 실제 난이도:  체감 난이도:
+// 실제 난이도: easy 체감 난이도: easy
 //* 문제이해
-//
+// 문자열 배열이 주어졌을 때, 가장 긴 prefix를 찾아내 리턴하는 문제
 //* 아이디어
-//
+// strs의 길이로 오름차순 정렬을 한다.
+// 가능한 가장 긴 prefix는 가장 길이가 짧은 str이 된다.(strs[0])
+// strs를 순회하며, prefix의 값을 줄여간다.
+// 1번이라도 prefix가 없다면 바로 return ''
 //* 풀이방법(순서도, 절차적프로그래밍)
-// 1.
-// 2.
-// 3.
+// 1. strs를 길이순으로 sort
+// 2. strs를 순회하며, 현재값과 현재 prefix의 공통된 prefix를 찾는다.
+// 2.1 없다면 바로 return '' 종료
+// 3. 있다면 현재 prefix값을 바꾸고, continue
 //* 시간복잡도
-// O()
+// sort: NlogN
+// 최악 N^2
 
 //* 복습필요여부
 //
@@ -23,22 +26,18 @@ function longestCommonPrefix(strs) {
     let prefix = "";
     for (let i = 0; i < a.length; i++) {
       let matcher = a.slice(0, i + 1);
-      if (b.indexOf(matcher) > -1) {
-        console.log("inside", prefix, matcher);
+      if (b.indexOf(matcher) === 0) {
         prefix = matcher;
       } else {
         return prefix;
       }
     }
-    console.log("here", prefix);
     return prefix;
   }
 
   let prefix = strs[0];
   for (let i = 1; i < strs.length; i++) {
     let cur = strs[i];
-    console.log(findMatch(prefix, cur), i);
-    console.log(prefix, cur);
     if (!findMatch(prefix, cur)) {
       return "";
     }
@@ -46,5 +45,5 @@ function longestCommonPrefix(strs) {
   }
   return prefix;
 }
-// console.log(longestCommonPrefix(["flower", "flow", "flight"])); // fl
+console.log(longestCommonPrefix(["reflower", "flow", "flight"])); // fl
 console.log(longestCommonPrefix(["ab", "a"])); // a
