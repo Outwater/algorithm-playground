162fce57dab35421bf5998ac84b5c411d82afbb5
Insoo-Seo <kocon135@gmail.com>
2022-02-10 01:35:24 +0900

    resolve: kakao2022 양궁대회 by DFS

--- Programmers/level2/(kakao2022)양궁대회byBFS.js
+++ Programmers/level2/(kakao2022)양궁대회byBFS.js
@@ -23,6 +23,15 @@
 // BFS를 통한 완전 탐색을 통해 해결시도
 // 라이언이 쏠 수 있는 방법은, 어피치가 쏜 것보다 1발 많이 쏘거나, 어피치가 쏜 것은 아예 쏘지 않는 방법이다.
 
+// dfs의 탈출조건
+// 1) n보다 더 많은 화살을 쏘았을 때
+// 2) 11개의 과녁판을 모두 체크하였을 때
+
+// idx=0 부터 시작하여 idx=10일 때까지 진행하며, 각각의 경우에 따라 appeach와 lion의 점수를 올려준다.
+// info[idx] > 0 는 lion(appeach보다 1발 더 쏘기) 혹은 appeach(쏘지않기)가 점수획득
+// info[idx] = 0 이면, lion(1발쏘기) 혹은 아무도 점수획득하지 않는 경우(쏘지않기) 발생
+
+// 모든과녀
 //* 풀이방법(순서도, 절차적프로그래밍)
 
 //* 시간복잡도
@@ -33,23 +42,90 @@
 
 function solution(n, info) {
   let maxDiff = 0;
-  let rion_shoots = Array.from({ length: 11 }, () => 0);
-  function dfs(sum_shoots_appeach, sum_shoots_lion, idx, shoot_cnt) {
-    let diff = sum_shoots_appeach - sum_shoots_lion;
+  let result = Array.from({ length: 11 }, () => 0);
+
+  function getLowestShoots(cur, res) {
+    for (let i = 0; i < 11; i++) {
+      let curScore = cur[10 - i];
+      let resultScore = res[10 - i];
+
+      if (curScore === resultScore) continue;
+      return curScore > resultScore ? cur : res;
+    }
+
+    return cur;
+  }
+
+  function dfs(
+    sum_shoots_appeach,
+    sum_shoots_lion,
+    idx,
+    shoot_cnt,
+    rion_shoots
+  ) {
     if (shoot_cnt > n) return;
 
-    if (idx === 10) {
-      if (shoot_cnt < n) {
-        rion_shoots[idx] = n - shoot_cnt;
+    if (idx > 10) {
+      rion_shoots[10] = n - shoot_cnt;
+
+      let diff = sum_shoots_lion - sum_shoots_appeach;
+      if (diff > maxDiff) {
+        result = rion_shoots;
+        maxDiff = diff;
+      }
+      if (diff === maxDiff) {
+        result = getLowestShoots(rion_shoots, result);
       }
       return;
     }
+
+    let rion_shoots_more_appeach = [...rion_shoots];
+    rion_shoots_more_appeach[idx] = info[idx] + 1;
+    let rion_shoots_no_shoot = [...rion_shoots];
+    rion_shoots_no_shoot[idx] = 0;
+
+    if (info[idx] > 0) {
+      dfs(
+        sum_shoots_appeach,
+        sum_shoots_lion + 10 - idx,
+        idx + 1,
+        shoot_cnt + info[idx] + 1,
+        rion_shoots_more_appeach
+      );
+      dfs(
+        sum_shoots_appeach + 10 - idx,
+        sum_shoots_lion,
+        idx + 1,
+        shoot_cnt,
+        rion_shoots_no_shoot
+      );
+    }
+    if (info[idx] === 0) {
+      dfs(
+        sum_shoots_appeach,
+        sum_shoots_lion + 10 - idx,
+        idx + 1,
+        shoot_cnt + info[idx] + 1,
+        rion_shoots_more_appeach
+      );
+      dfs(
+        sum_shoots_appeach,
+        sum_shoots_lion,
+        idx + 1,
+        shoot_cnt,
+        rion_shoots_no_shoot
+      );
+    }
   }
 
-  dfs(0, 0, 0, 0, rion_shoots);
+  dfs(0, 0, 0, 0, result);
+  if (maxDiff === 0) {
+    return [-1];
+  }
+  return result;
 }
 
 // console.log(solution(5, [2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0])); // [0,2,2,0,1,0,0,0,0,0,0]
 // console.log(solution(1, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])); // [-1]
-// console.log(solution(9, [0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1])); // [1,1,2,0,1,2,2,0,0,0,0]
+console.log(solution(9, [0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1])); // [1,1,2,0,1,2,2,0,0,0,0]
 // console.log(solution(10, [0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3])); //	[1,1,1,1,1,1,1,1,0,0,2]
